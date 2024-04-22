acc476108a5fac0813a4467cae8891a0bad9866e
dongjaee <t01023919874@gmail.com>
2024-04-22 17:58:02 +0900

    remove PlayingVideo function

--- front/lib/functions/PlayingVideo.dart
+++ front/lib/functions/PlayingVideo.dart
@@ -1,53 +1,52 @@
-import 'dart:ui' as ui;
-import 'dart:html' as html;
-
-import 'package:flutter/material.dart';
-// dart:html 라이브러리를 직접 사용하지 않고, UI 플랫폼 뷰를 통해 접근
-
-class PlayingVideo extends StatefulWidget {
-  final String videoUrl;
-
-  PlayingVideo({Key? key, required this.videoUrl}) : super(key: key);
-
-  @override
-  _PlayingVideoState createState() => _PlayingVideoState();
-}
-
-class _PlayingVideoState extends State<PlayingVideo> {
-  late String viewType;
-
-  @override
-  void initState() {
-    super.initState();
-    initViewType();
-  }
-
-  @override
-  void didUpdateWidget(PlayingVideo oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.videoUrl != widget.videoUrl) {
-      // videoUrl이 변경될 경우, 새로운 viewType을 생성하여 플랫폼 뷰를 재등록합니다.
-      initViewType();
-    }
-  }
-
-  void initViewType() {
-    // viewType을 현재 시간을 기반으로 재정의하여 고유하게 만듭니다.
-    viewType = 'video-view-type-${DateTime.now().millisecondsSinceEpoch}';
-    // 플랫폼 뷰에서 사용할 HTML <video> 태그를 등록합니다.
-    ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
-      final videoElement = html.VideoElement()
-        ..src = widget.videoUrl
-        ..autoplay = true
-        ..controls = true
-        ..style.width = '100%'
-        ..style.height = '100%';
-      return videoElement;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return HtmlElementView(viewType: viewType);
-  }
-}
+// import 'dart:ui' as ui;
+// import 'dart:html' as html;
+//
+// import 'package:flutter/material.dart';
+//
+// class PlayingVideo extends StatefulWidget {
+//   final String videoUrl;
+//
+//   PlayingVideo({Key? key, required this.videoUrl}) : super(key: key);
+//
+//   @override
+//   _PlayingVideoState createState() => _PlayingVideoState();
+// }
+//
+// class _PlayingVideoState extends State<PlayingVideo> {
+//   late String viewType;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     initViewType();
+//   }
+//
+//   @override
+//   void didUpdateWidget(PlayingVideo oldWidget) {
+//     super.didUpdateWidget(oldWidget);
+//     if (oldWidget.videoUrl != widget.videoUrl) {
+//       // videoUrl이 변경될 경우, 새로운 viewType을 생성하여 플랫폼 뷰를 재등록합니다.
+//       initViewType();
+//     }
+//   }
+//
+//   void initViewType() {
+//     // viewType을 현재 시간을 기반으로 재정의하여 고유하게 만듭니다.
+//     viewType = 'video-view-type-${DateTime.now().millisecondsSinceEpoch}';
+//     // 플랫폼 뷰에서 사용할 HTML <video> 태그를 등록합니다.
+//     ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
+//       final videoElement = html.VideoElement()
+//         ..src = widget.videoUrl
+//         ..autoplay = true
+//         ..controls = true
+//         ..style.width = '100%'
+//         ..style.height = '100%';
+//       return videoElement;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return HtmlElementView(viewType: viewType);
+//   }
+// }
