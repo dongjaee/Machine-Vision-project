52cb10ece7aff7b803ba0a58d5ce3b88c04e9c2d
dongjaee <t01023919874@gmail.com>
2024-04-22 17:54:50 +0900

    create uploadVideo function

--- front/lib/functions/ApiService.dart
+++ front/lib/functions/ApiService.dart
@@ -1,1 +1,27 @@
-//TODO: API함수 구현
+import 'dart:typed_data';
+
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+
+class ApiService {
+  Future<void> uploadVideo(Uint8List fileBytes, String fileName) async {
+    var uri = Uri.parse('http://yourserver.com/upload-video/');
+    var request = http.MultipartRequest('POST', uri)
+      ..files.add(http.MultipartFile.fromBytes(
+        'video', // 서버에서 기대하는 필드 이름
+        fileBytes,
+        filename: fileName,
+      ));
+
+    // 요청 전송
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('Upload successful');
+    } else {
+      print('Upload failed');
+    }
+  }
+}
