ccc52b6faf98977008b5f64aa7137c8976400832
dongjaee <t01023919874@gmail.com>
2024-04-27 13:58:01 +0900

    feature: create uploading video logic

--- front/lib/functions/PlayingVideo.dart
+++ front/lib/functions/PlayingVideo.dart
@@ -50,3 +50,85 @@
 //     return HtmlElementView(viewType: viewType);
 //   }
 // }
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:front/functions/ApiService.dart';
+
+class PlayingVideo extends StatefulWidget {
+  @override
+  _PlayingVideoState createState() => _PlayingVideoState();
+}
+
+class _PlayingVideoState extends State<PlayingVideo> {
+  VideoPlayerController? _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    ApiService.onVideoUrlChanged = () {
+      setState(() {
+        initializeVideo(); // uploadedVideoUrl 변경 시 초기화 함수 호출
+      });
+    };
+  }
+
+  Future<void> initializeVideo() async {
+    try {
+      if (ApiService.uploadedVideoUrl != null) {
+        _controller?.dispose(); // 기존 컨트롤러 해제
+        _controller = VideoPlayerController.network(ApiService.uploadedVideoUrl!);
+        await _controller!.initialize();
+        setState(() {
+          _isLoading = false;
+        });
+        _controller!.play(); // 동영상 자동 재생
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      print('Error initializing video: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : ApiService.uploadedVideoUrl != null &&
+        _controller != null &&
+        _controller!.value.isInitialized
+        ? AspectRatio(
+      aspectRatio: _controller!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(_controller!),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: VideoProgressIndicator(
+              _controller!,
+              allowScrubbing: true,
+            ),
+          ),
+        ],
+      ),
+    )
+        : Center(
+      child: Text(
+        "동영상 업로드를 기다리는 중...",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
